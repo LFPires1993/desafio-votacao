dc908f26b0a3b819978357733d18ea9720cd458e
LFPires1993 <lfpires1993@gmail.com>
2024-11-05 11:06:58 -0300

    Create app

---
+++ src/test/java/com/lucas/pires/challenge/voting/sistem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.pires.challenge.voting.sistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
