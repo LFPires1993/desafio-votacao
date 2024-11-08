a4b699a711a5d1072c8ec18998c40ca62c0e463e
LFPires1993 <lfpires1993@gmail.com>
2024-11-08 16:26:37 -0300

    Finalize last version

--- src/main/java/com/lucas/pires/challenge/voting/system/dtos/adapters/VoteAdapter.java
+++ src/main/java/com/lucas/pires/challenge/voting/system/dtos/adapters/VoteAdapter.java
@@ -1,6 +1,5 @@
 package com.lucas.pires.challenge.voting.system.dtos.adapters;
 
-import com.lucas.pires.challenge.voting.system.utils.enums.VoteOptions;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public class VoteAdapter {
